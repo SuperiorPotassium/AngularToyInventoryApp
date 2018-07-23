6bfae8dd162409031828153ef4358b911b101307
SuperiorPotassium <Adrian.Boloix@gmail.com>
2018-07-23 12:28:27 -0400

    Add files via upload

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,1 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Toy } from '../toy';
+import { ToyService } from '../toy.service';
 
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
+})
+export class DashboardComponent implements OnInit {
+  toys: Toy[] = [];
+
+  constructor(private toyService: ToyService) { }
+
+  ngOnInit() {
+    this.getToys();
+  }
+
+  getToys(): void {
+    this.toyService.getToys()
+      .subscribe(toys => this.toys = toys.slice(1, 5));
+  }
+}
